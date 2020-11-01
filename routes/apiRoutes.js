685be3b2f1c6f9d59faf45c66fbae8f2f78173a1
Nardacyon <63487094+Nardacyon@users.noreply.github.com>
2020-11-01 01:28:40 -0800

    corrected syntax error

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -17,7 +17,7 @@ module.exports = function (app) {
     });
     
     app.put("/api/workouts/:id", ({ body, params }, res) => {
-        Workout.findByIdAndUpdate(
+        db.Workout.findByIdAndUpdate(
             params.id,
             { $push: { exercises: body } },
             { new: true, runValidators: true }
